1a6a79755414b04fe046bb0e5d14ca51e5a74e7b
Sakshiiiipatil <145013373+Sakshiiiipatil@users.noreply.github.com>
2023-10-01 00:31:46 +0530

    Sakshi Patil Update DuplicateParenthesesCode.java

--- Stacks/DuplicateParenthesesCode.java
+++ Stacks/DuplicateParenthesesCode.java
@@ -49,3 +49,22 @@ public class DuplicateParenthesesCode {
 
     }
 }
+
+
+/*isDuplicate method:
+
+The isDuplicate method takes a string str as input and checks for duplicate parentheses.
+It uses a stack (Stack<Character> s) to keep track of opening and closing parentheses.
+It iterates through the characters of the input string from left to right.
+When an opening parenthesis '(' is encountered, it is pushed onto the stack.
+When a closing parenthesis ')' is encountered, it checks for the presence of duplicate parentheses by counting the number of characters between the current closing parenthesis and the nearest opening parenthesis. It keeps popping characters from the stack until an opening parenthesis is encountered.
+If the count of characters between the opening and closing parentheses is less than 1 (indicating no characters in between), it returns true to indicate the presence of duplicate parentheses. Otherwise, it proceeds by popping the opening parenthesis from the stack.
+Finally, if the loop completes without finding duplicate parentheses, the method returns false.
+main method:
+
+In the main method, two example strings str1 and str2 are defined.
+The isDuplicate method is called with each string as input.
+The program prints the result, where true indicates the presence of duplicate parentheses, and false indicates the absence of duplicate parentheses.
+Output:
+
+The program checks and prints whether each input string contains duplicate parentheses.*/
