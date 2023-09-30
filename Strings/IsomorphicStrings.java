da3f9cb54ba29bc2a65e7eccbbe434b97e9093be
Arpita Sawant <97539573+arpitasawant@users.noreply.github.com>
2023-10-01 00:36:00 +0530

    Create IsomorphicStrings.java

---
+++ Strings/IsomorphicStrings.java
@@ -0,0 +1,2 @@
+/*Check if two given Strings are Isomorphic to each other
+Two strings str1 and str2 are called isomorphic if there is a one-to-one mapping possible for every character of str1 to every character of str2. And all occurrences of every character in ‘str1’ map to the same character in ‘str2’. */
